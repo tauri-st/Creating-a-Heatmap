9e2092a01b7ffb7d6593ef46913a19986fa3d7f9
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-24 00:55:55 +0000

    Add comment to label pivot variable

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import seaborn as sns
 with open("tips.csv", "r") as csvfile:
   tips = pd.read_csv(csvfile)
 
+#define the dataframe
 tips_pivoted = tips.pivot_table(values="tip", index=["size"], columns=["time"])
+
