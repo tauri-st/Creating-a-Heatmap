bb58d6cb458f2cc8f1ac085fb43ec0f6910c9901
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-24 19:09:39 +0000

    Fix ylim function to run before savefig and set greater limit

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ tips_pivoted = tips.pivot_table(values="tip", index=["size"], columns=["time"])
 #creat a heat map using the dataframe with annotations and a coolwarm color palette
 fig = sns.heatmap(tips_pivoted, annot=True, cmap="coolwarm")
 
+fig.set_ylim(0,5)
+
 plt.xlabel("Meal Time")
 plt.ylabel("Size of Group")
 
 plt.title("Average Tips by Group Size and Meal Time")
 
 plt.savefig("tips-by-group-and-time.png")
-
-fig.set_ylim(0,6)
